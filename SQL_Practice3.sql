3bd8dbb02a3e5cbccdfddac5923eda338a0fc56d
Your Name <ahmet.dikbayir@gmail.com>
2024-03-13 22:47:30 +0300

    update

---
+++ SQL_Practice3.sql
@@ -0,0 +1,17 @@
+CREATE TABLE meslekler
+(
+id int PRIMARY KEY, 
+isim VARCHAR(50), 
+soyisin VARCHAR(50), 
+meslek CHAR(9), 
+maas int
+);
+
+INSERT INTO meslekler VALUES (1, 'Ali', 'Can', 'Doktor', '20000' ); 
+INSERT INTO meslekler VALUES (2, 'Veli', 'Cem', 'Mühendis', '18000'); 
+INSERT INTO meslekler VALUES (3, 'Mine', 'Bulut', 'Avukat', '17008'); 
+INSERT INTO meslekler VALUES (4, 'Mahmut', 'Bulut', 'Ögretmen', '15000'); 
+INSERT INTO meslekler VALUES (5, 'Mine', 'Yasa', 'Teknisyen', '13008'); 
+INSERT INTO meslekler VALUES (6, 'Veli', 'Yilmaz', 'Hemşire', '12000'); 
+INSERT INTO meslekler VALUES (7, 'Ali', 'Can', 'Marangoz', '10000' ); 
+INSERT INTO meslekler VALUES (8, 'Veli', 'Cem', 'Tekniker', '14000');
