7cbe170a31f9a2320de1402af93bdc76b9d3434f
Your Name <ahmet.dikbayir@gmail.com>
2024-03-15 23:34:38 +0300

    day 7 added

--- SQL_Practice_Own_By_Own.sql
+++ SQL_Practice_Own_By_Own.sql
@@ -2,11 +2,13 @@ SELECT *
 FROM developers
 ORDER BY salary DESC;
 
---Find the developers name who has a second highest salary
-SELECT name
+--Find the developers name and city who has a second highest salary
+SELECT name, city
 FROM developers
 WHERE salary= (SELECT MAX(salary)
 FROM developers
 WHERE salary< (SELECT MAX(salary) FROM developers))
+
+
 SELECT *
 FROM testers
