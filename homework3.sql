a3ce169dbe084a79bd34dff4a4843337c8c90590
Your Name <ahmet.dikbayir@gmail.com>
2024-03-17 21:53:49 +0300

    homework 3 finished

--- homework3.sql
+++ homework3.sql
@@ -81,3 +81,63 @@ SELECT *
 FROM personels
 WHERE email !~* 'gmail';
 
+-- 11) id'si 3,5,7 ve 9 olan personel bilgilerini listeleyiniz.
+SELECT *
+FROM personels
+WHERE id IN (3,5,7,9);
+
+-- 12) yaşı 39,48 ve 54 olmayan personel bilgilerini listeleyiniz.
+SELECT *
+FROM personels
+WHERE age NOT IN (39, 48, 54);
+
+-- 13) yaşı 30 ve 40 arasında olan personel bilgilerini listeleyiniz.
+SELECT *
+FROM personels
+WHERE age BETWEEN 30 AND 40;
+
+-- 14) yaşı 30 ve 40 arasında olmyan personel bilgilerini listeleyiniz.
+SELECT *
+FROM personels
+WHERE age NOT BETWEEN 30 AND 40;
+
+-- 15) emaili olmayan personel bilgilerini listeleyiniz.
+SELECT *
+FROM personels
+WHERE email '';
+
+--Added new data for testing age criteria and email criteria
+
+INSERT INTO personels(id, name, age, email) VALUES (12, 'Deniz', 45, '');
+INSERT INTO personels(id, name, age, email) VALUES (13, 'Ege', 40, '');
+
+-- 16) emaili olan personel bilgilerini listeleyiniz.
+SELECT *
+FROM personels
+WHERE email  ='';
+
+-- 17) personel bilgilerini yaşa göre sıralayınız.
+SELECT *
+FROM personels
+ORDER BY age;
+
+-- 18) personel bilgilerini maaşa göre sıralayınız.
+SELECT *
+FROM personels
+ORDER BY salary;
+
+-- 19) personelin yaşlarını büyükten küçüğe doğru sıralayınız.
+SELECT age
+FROM personels
+ORDER BY age DESC;
+
+-- 20) personelin maaşlarını büyükten küçüğe doğru sıralayınız.
+SELECT salary
+FROM personels
+ORDER BY salary DESC;
+
+-- 21) En yüksek maaş olan ilk 3 personel bilgilerini sıralayınız
+SELECT *
+FROM personels
+ORDER BY salary DESC
+LIMIT 3;
